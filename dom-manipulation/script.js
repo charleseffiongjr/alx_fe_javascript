087f5578543ed5df3a66b41060def814d3d9a1a3
Charles Effiong <charleseffiong28@gmail.com>
2025-02-15 02:08:27 +0100

    dom-manipulation

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,13 +1,30 @@
 let quotes = [
-    {
-        text: "", category: ""
-    }
-]
+    { text: "The journey of a thousand miles begins with one step.", category: "Motivation"},
+    { text: "I'm not lazy, I'm on energy-saving mode.", category: "Humor"}
+];
+const quoteDisplay = document.getElementById("quoteDisplay");
+const newQuoteButton = document.getElementById("newQuote");
+const newQuoteText = document.getElementById("newQuoteText");
+const newQuoteCategory = document.getElementById("newQuoteCategory");
 
 function showRandomQuote(){
     const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex];
     random.innerHTML = "" 
 
+
+}
+function displayAllQuotes() {
+    quoteListElem.innerHTML = "";
+    quotes.forEach((quote, index) => {
+        const quoteItem = document.createElement("div");
+        quoteItem.classList.add("quote-item");
+        quoteItem.innerHTML = `
+            <p>"${quote.text}"</p>
+            <p class="category">- ${quote.category}</p>
+        `;
+        quoteListElem.appendChild(quoteItem);
+    });
 }
 function createAddQuoteForm(){
 
